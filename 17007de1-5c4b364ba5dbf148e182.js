f09e9bc4ceb1b549998473c9034d48f91accfa77
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-01-11 11:25:09 -0500

    Updates

---
+++ 17007de1-5c4b364ba5dbf148e182.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{h1kF:function(n,t,e){"use strict";e.d(t,"a",(function(){return i}));var o=e("Lnxd");function i(n){return Object(o.a)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M3.147 9l5 5h.707l5-5-.707-.707L9 12.439V2H8v10.44L3.854 8.292 3.147 9z"}}]})(n)}}}]);
+//# sourceMappingURL=17007de1-5c4b364ba5dbf148e182.js.map
